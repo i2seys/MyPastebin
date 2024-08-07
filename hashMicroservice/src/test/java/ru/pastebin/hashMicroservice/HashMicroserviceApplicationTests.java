c09fa4ecacca16c4cb6ea9823257325b1a3b34d0
i2seys <ilya-savenkov.savenkov@yandex.ru>
2024-08-07 19:17:21 +0300

    [hashMicroservice] Add empty hash microservice
    Добавил пустой проект с Hash microservice.

---
+++ hashMicroservice/src/test/java/ru/pastebin/hashMicroservice/HashMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pastebin.hashMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
